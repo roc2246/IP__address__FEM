8fe32ced4e336173e71ce12e22a1d48d3abde1d3
Riley <riley.childs@yahoo.com>
2023-04-30 11:47:28 -0400

    IP search function now works with text input

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-async function getIPaddress(IPaddress) {
-  const jsonData = `https://geo.ipify.org/api/v2/country,city?apiKey=at_KcMC0GkoBIWgrY2WWAWXa09RpazKh&ipAddress=${IPaddress}`;
+const IPinput = document.getElementsByClassName("get-info__input")[0];
+const submitQuery = document.getElementsByClassName("get-info__submit")[0];
+
+async function getIPaddress() {
+  const jsonData = `https://geo.ipify.org/api/v2/country,city?apiKey=at_KcMC0GkoBIWgrY2WWAWXa09RpazKh&ipAddress=${IPinput.value}`;
+  console.log(jsonData)
   const response = await fetch(jsonData);
   const data = await response.json();
   return data;
 }
 
-getIPaddress().then((data) => {
-  console.log(data);
-});
+submitQuery.onclick = (e) => {
+  e.preventDefault();
+
+  getIPaddress().then((data) => {
+    console.log(data);
+  });
+};
