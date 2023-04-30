f06a6faac0380284c5f0c0bf892ed10cc3fb0bc5
Riley <riley.childs@yahoo.com>
2023-04-30 12:16:14 -0400

    Search results now display in  .ip-location

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const getAddress = {
-    APIkey: "at_KcMC0GkoBIWgrY2WWAWXa09RpazKh",
-    IPinput: document.getElementsByClassName("get-info__input")[0],
-    submitQuery: document.getElementsByClassName("get-info__submit")[0],
-    getData: async function getIPaddress() {
-        const jsonData = `https://geo.ipify.org/api/v2/country,city?apiKey=${this.APIkey}&ipAddress=${this.IPinput.value}`;
-        const response = await fetch(jsonData);
-        const data = await response.json();
-        return data;
-      }
-}
+  APIkey: "at_KcMC0GkoBIWgrY2WWAWXa09RpazKh",
+  IPinput: document.getElementsByClassName("get-info__input")[0],
+  submitQuery: document.getElementsByClassName("get-info__submit")[0],
+  getData: async function getIPaddress() {
+    const jsonData = `https://geo.ipify.org/api/v2/country,city?apiKey=${this.APIkey}&ipAddress=${this.IPinput.value}`;
+    const response = await fetch(jsonData);
+    const data = await response.json();
+    return data;
+  },
+};
 
+const result = {
+  IP: document.getElementsByClassName("ip-location__stat--info")[0],
+  location: document.getElementsByClassName("ip-location__stat--info")[1],
+  timezone: document.getElementsByClassName("ip-location__stat--info")[2],
+  ISP: document.getElementsByClassName("ip-location__stat--info")[3],
+};
 getAddress.submitQuery.onclick = (e) => {
   e.preventDefault();
-  getAddress.IPinput.value = ""
- 
+
   getAddress.getData().then((data) => {
-    console.log(data);
+    const { location, isp } = data;
+    console.log(isp);
+
+    result.IP.innerHTML = getAddress.IPinput.value;
+    result.location.innerHTML = `${location.city}, ${location.region} ${location.postalCode}`;
+    result.timezone.innerHTML = `UTC ${location.timezone}`;
+    result.ISP.innerHTML = isp;
+
+    getAddress.IPinput.value = "";
   });
 };
