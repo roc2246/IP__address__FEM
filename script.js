6548d0fd60654d56f9e607332214e8307bf3b866
Riley <riley.childs@yahoo.com>
2023-04-30 11:53:58 -0400

    Refactored IP search code into object

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const IPinput = document.getElementsByClassName("get-info__input")[0];
-const submitQuery = document.getElementsByClassName("get-info__submit")[0];
-
-async function getIPaddress() {
-  const jsonData = `https://geo.ipify.org/api/v2/country,city?apiKey=at_KcMC0GkoBIWgrY2WWAWXa09RpazKh&ipAddress=${IPinput.value}`;
-  console.log(jsonData)
-  const response = await fetch(jsonData);
-  const data = await response.json();
-  return data;
+const getAddress = {
+    APIkey: "at_KcMC0GkoBIWgrY2WWAWXa09RpazKh",
+    IPinput: document.getElementsByClassName("get-info__input")[0],
+    submitQuery: document.getElementsByClassName("get-info__submit")[0],
+    getData: async function getIPaddress() {
+        const jsonData = `https://geo.ipify.org/api/v2/country,city?apiKey=${this.APIkey}&ipAddress=${this.IPinput.value}`;
+        const response = await fetch(jsonData);
+        const data = await response.json();
+        return data;
+      }
 }
 
-submitQuery.onclick = (e) => {
+getAddress.submitQuery.onclick = (e) => {
   e.preventDefault();
-
-  getIPaddress().then((data) => {
+ 
+  getAddress.getData().then((data) => {
     console.log(data);
   });
 };
