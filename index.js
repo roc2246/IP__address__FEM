7ccb332915ceb3d49a2241bc57e195dd11495e5a
Riley <riley.childs@yahoo.com>
2023-05-20 17:02:22 -0400

    Created route for api requests

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const portNo = process.env.PORT || 3000;
+
+const test = "192.168.0.1";
+
+const getAddress = {
+  APIkey: "at_KcMC0GkoBIWgrY2WWAWXa09RpazKh",
+  getData: async function getIPaddress() {
+    const jsonData = `https://geo.ipify.org/api/v2/country,city?apiKey=${this.APIkey}&ipAddress=${this.IPinput.value}`;
+    const response = await fetch(jsonData);
+    const data = await response.json();
+    return data;
+  },
+};
+
+app.get("/address", (req, res) => {
+  try {
+    fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_KcMC0GkoBIWgrY2WWAWXa09RpazKh&ipAddress=192.168.0.1`
+    )
+      .then((response) => response.json())
+      .then((data) => res.json(data));
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.listen(portNo, () => {
+  console.log(`Server running on http://127.0.0.1:${portNo}
+for data, go to http://127.0.0.1:${portNo}/address`);
+});
