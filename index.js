fd3a1be57a84b70eb3a6cb19233bc74dc0db25b3
Riley <riley.childs@yahoo.com>
2023-05-21 10:39:30 -0400

    Removed unused code from index.js

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 
 const portNo = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-const getAddress = {
-  APIkey: process.env.API_KEY,
-  getData: async function getIPaddress() {
-    const jsonData = `https://geo.ipify.org/api/v2/country,city?apiKey=${this.APIkey}&ipAddress=${this.IPinput.value}`;
-    const response = await fetch(jsonData);
-    const data = await response.json();
-    return data;
-  },
-};
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/address/:address", (req, res) => {
   try {
     fetch(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=${getAddress.APIkey}&ipAddress=${req.params.address}`
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.API_KEY}&ipAddress=${req.params.address}`
     )
       .then((response) => response.json())
       .then((data) => res.json(data));
